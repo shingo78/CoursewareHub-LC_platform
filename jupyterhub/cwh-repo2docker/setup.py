0538cc1db5d486df0c04bc31f53c3a15324a7b49
Shingo Nakagawa <shingo.nakagawa@gmail.com>
2023-02-15 18:30:39 +0900

    Fix setup.py, enable include_package_data

--- jupyterhub/cwh-repo2docker/setup.py
+++ jupyterhub/cwh-repo2docker/setup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import print_function
-
-import os
 import sys
 
 v = sys.version_info
@@ -12,40 +9,19 @@ if v[:2] < (3,3):
     print(error, file=sys.stderr)
     sys.exit(1)
 
-
-if os.name in ('nt', 'dos'):
-    error = "ERROR: Windows is not supported"
-    print(error, file=sys.stderr)
-
-# At least we're on the python version we need, move on.
-
 from setuptools import setup
 
-pjoin = os.path.join
-here = os.path.abspath(os.path.dirname(__file__))
-
-from setuptools.command.bdist_egg import bdist_egg
-class bdist_egg_disabled(bdist_egg):
-    """Disabled version of bdist_egg
-    Prevents setup.py install from performing setuptools' default easy_install,
-    which it should never ever do.
-    """
-    def run(self):
-        sys.exit("Aborting implicit building of eggs. Use `pip install .` to install from source.")
-
 setup_args = dict(
-    name                = 'cwh-repo2docker',
-    packages            = ['cwh_repo2docker'],
-    version             = '0.1.0',
-    platforms           = "Linux",
-    install_requires    = [
+    name                 = 'cwh-repo2docker',
+    packages             = ['cwh_repo2docker'],
+    version              = '0.1.0',
+    platforms            = "Linux",
+    include_package_data = True,
+    install_requires     = [
         "coursewareuserspawner",
         "jupyterhub~=3.1",
         "aiodocker",
-        'aiohttp'],
-    cmdclass = {
-        'bdist_egg': bdist_egg if 'bdist_egg' in sys.argv else bdist_egg_disabled,
-    }
+        'aiohttp']
 )
 
 
