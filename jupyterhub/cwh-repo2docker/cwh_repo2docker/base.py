5ca2a06f4d8952907faa004bda9f43fc8c208e64
shingo78 <shingo.nakagawa@gmail.com>
2024-03-05 15:58:16 +0900

    Add BaseHandler.get_json_body

--- jupyterhub/cwh-repo2docker/cwh_repo2docker/base.py
+++ jupyterhub/cwh-repo2docker/cwh_repo2docker/base.py
@@ -37,3 +37,16 @@ class BaseHandler(web.RequestHandler):
         )
         return ns
 
+    def get_json_body(self):
+        """Return the body of the request as JSON data."""
+        if not self.request.body:
+            return None
+        body = self.request.body.strip().decode('utf-8')
+        try:
+            model = json.loads(body)
+        except Exception:
+            self.log.debug("Bad JSON: %r", body)
+            self.log.error("Couldn't parse JSON", exc_info=True)
+            raise web.HTTPError(400, 'Invalid JSON in body of request')
+        return model
+
